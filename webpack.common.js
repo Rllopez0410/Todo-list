c3071125f4bf989f1726c3c5fd29ea57fe1b1697
Roger Lopez <rllopez0410@gmail.com>
2023-11-02 02:33:33 -0400

    configuring webpack

--- webpack.common.js
+++ webpack.common.js
@@ -7,4 +7,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
